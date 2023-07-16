4536f201d8189763801d12e85bc5a703bc1cc83c
harriszk <zacharykeatonharris@gmail.com>
2023-07-16 13:31:30 -0400

    Added third party licenses

---
+++ src/logger.cc
@@ -0,0 +1,18 @@
+// @file logger.cc
+// Created on 07/16/2023
+// 
+// 
+// 
+// © 2023 by Zachary Harris (zacharykeatonharris@gmail.com)
+#include "logger.h"
+
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+std::shared_ptr<spdlog::logger> Logger::logger_;
+
+void Logger::Initialize() {
+  spdlog::set_pattern("%^[%T] %n: %v%$");
+  logger_ = spdlog::stdout_color_mt("LOG");
+  logger_->set_level(spdlog::level::trace);
+}
+
