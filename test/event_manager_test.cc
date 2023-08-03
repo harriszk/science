f3af873d7a343771b841b41bf31631a627a641b5
harriszk <zacharykeatonharris@gmail.com>
2023-08-03 17:32:55 -0400

    Added event manager test stubs

---
+++ test/event_manager_test.cc
@@ -0,0 +1,39 @@
+// @file event_manager_test.cc
+// Created on 08/03/2023
+// 
+// 
+// 
+// © 2023 by Zachary Harris (zacharykeatonharris@gmail.com)
+#include <gtest/gtest.h>
+
+TEST(EventManagerTest, SubscribeTest) {
+  FAIL();
+}
+
+TEST(EventManagerTest, EventDispatchTest) {
+  FAIL();
+}
+
+TEST(EventManagerTest, UnsubscribeTest) {
+  FAIL();
+}
+
+TEST(EventManagerTest, UnsubscribeAllTest) {
+  FAIL();
+}
+
+TEST(EventManagerTest, SubscribeMultipleHandlersTest) {
+  FAIL();
+}
+
+TEST(EventManagerTest, UnsubscribeFromMultipleEventsTest) {
+  FAIL();
+}
+
+TEST(EventManagerTest, EventOrderingTest) {
+  FAIL();
+}
+
+TEST(EventManagerTest, DispatchToDerivedEventHandlerTest) {
+  FAIL();
+}
